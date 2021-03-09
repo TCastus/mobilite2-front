bc9d6032110142df8e1f5dd8ab4ccd33c20279bb
Louis Gombert <louisgombert@gmail.com>
2021-03-09 15:53:32 +0100

    Connect to the DB, setup project architecture

---
+++ src/Component/APITest.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {getHealthCheck} from "../Request/uni_request";
+import {getDefaultErrorMessage} from "../Request/error_handling";
+import {Typography} from "@material-ui/core";
+
+/**
+ * A component used to test the connectivity with the API
+ */
+export default class TestApi extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: []
+        };
+    }
+
+    componentDidMount() {
+        getHealthCheck().then(res => {
+            const message = res.data;
+            this.setState({message});
+        }).catch( err => {
+            console.log(getDefaultErrorMessage(err));
+        });
+    }
+
+    render() {
+        return (
+            <Typography>
+                Server says : &quot;{this.state.message.message}&quot;
+            </Typography>
+        );
+    }
+
+}

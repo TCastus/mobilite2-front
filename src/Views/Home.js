19f93c5fe7b25f8104d9e86fa6463e4ecbc14bd5
s-ter <72151432+s-ter@users.noreply.github.com>
2021-03-17 14:14:06 +0100

    Home.js : start of creation Background

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import '../Assets/Style/App.css';
 import TestApi from "../Component/APITest";
+import { makeStyles } from '@material-ui/core/styles';
+import {CssBaseline} from "@material-ui/core";
 
+
+const useStyles = makeStyles(() => ({
+    root: {
+        minHeight : '100vh',
+        backgroundImage: `url(${'./../Assets/images/Carte.jpg'})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+    },
+}));
 
 function Home() {
+
+    const classes = useStyles();
     return (
-        <div className="Accueil">
+        <div className={classes.root}>
             <h1> Accueil </h1>
-            <Paper style={styles.paperContainer}>
-                <Paper />
-            </Paper>
+            <CssBaseline/>
             <TestApi/>
         </div>
     );
 }
 
 export default Home;
+

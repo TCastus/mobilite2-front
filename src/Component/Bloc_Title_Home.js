f4bb232c38e03fb8e539ca8eb268d019e722c5b5
s-ter <72151432+s-ter@users.noreply.github.com>
2021-03-17 15:56:41 +0100

    j'avais oublié Bloc_Title_Home.js

---
+++ src/Component/Bloc_Title_Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import{ makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles(() => ({
+    Titre: {
+        height: '20vh',
+        color: 'lightcyan',
+        background: 'rgba(100,100,100,100)',
+        margin: '20px',
+        padding: 2,
+        textAlign: 'center',
+    },
+
+}));
+
+export default function Titre (){
+    const classes = useStyles();
+    return(
+        <div className={classes.Titre}>
+            <h1> Mobilités Internationales </h1>
+            <h2> Découvrer les avis de vos étudiants </h2>
+        </div>
+    );
+}

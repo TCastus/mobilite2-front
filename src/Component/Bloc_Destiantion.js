5cd8967b46e41528a150ad4ea60b65ca60455084
Louis Gombert <louisgombert@gmail.com>
2021-03-17 21:17:05 +0100

    Better routing file

--- src/Component/Bloc_Destiantion.js
+++ src/Component/Bloc_Destiantion.js
@@ -2,7 +2,7 @@ import React from 'react';
 import{ makeStyles } from "@material-ui/core/styles";
 
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
 
     Destination: {
         height: '40vh',

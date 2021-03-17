45a2574af0f2b90c99ae83b1509de399bf4f29bd
Estelle <esmonier01@gmail.com>
2021-03-17 10:15:36 +0100

    debut home page (background marche pas)

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -7,6 +7,9 @@ function Home() {
     return (
         <div className="Accueil">
             <h1> Accueil </h1>
+            <Paper style={styles.paperContainer}>
+                <Paper />
+            </Paper>
             <TestApi/>
         </div>
     );

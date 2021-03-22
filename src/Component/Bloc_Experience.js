c59ffcc002e3e912dfdf59f1806f698fc0791b1f
Louis Gombert <louisgombert@gmail.com>
2021-03-22 16:36:09 +0100

    continue homepage

--- src/Component/Bloc_Experience.js
+++ src/Component/Bloc_Experience.js
@@ -17,17 +17,6 @@ const useStyles = makeStyles((theme) => ({
 export default function Experience () {
     const classes = useStyles();
     return (
-        <div className={classes.Experience}>
-            <Typography variant={'h3'}>Partagez votre expérience</Typography>
-            <Typography variant={'h4'}>
-                Vous revenez d&apos;échange ? <br/>
-                Aidez les étudiants dans leur choix en<br/>
-                partageant votre expérience et en<br/>
-                répondant à leurs questions.
-            </Typography>
-            <Button variant="contained" color="secondary" href="experience">
-                Donner mon avis
-            </Button>
-        </div>
+
     );
 }

f1a100394716642c07974fe7ceed1b1af68815fa
s-ter <72151432+s-ter@users.noreply.github.com>
2021-04-28 11:08:01 +0200

    Branche PartageExperience: --------Experience.js (dans Views)-------- - titre - bloc formulaire - star rating - bouton submit - mise en page (background, couleurs, alignements, police...)
    -------- style.css (dans Assets/Style) -------
    - police
    - background

--- src/Views/Experience.js
+++ src/Views/Experience.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import styles from'../Assets/Style/App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
 import {blueGrey} from "@material-ui/core/colors";

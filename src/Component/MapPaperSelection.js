f7a399ab9eade4e55de66b4012a0bc272f3521c0
s-ter <72151432+s-ter@users.noreply.github.com>
2021-05-05 23:18:42 +0200

    Home.js : - mise en page version 2 (avant ct bien moche) - modif couleurs, police - ajouts Box - que des éléments de material ui
    MapPaperSelection.js:
    - modif couleurs
    - ajout select
    - modif police
    - ajout "université" en vertical

--- src/Component/MapPaperSelection.js
+++ src/Component/MapPaperSelection.js
@@ -3,12 +3,14 @@ import React from 'react';
 import{ makeStyles } from "@material-ui/core/styles";
 import {Container, Grid, Paper, Typography} from "@material-ui/core";
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 
 const useStyles = makeStyles((theme) => ({
     paper: {
-        background: '#77c0ff',
-        margin: theme.spacing(5),
+        background: 'cadetblue',
+        margin: theme.spacing(2),
         padding: theme.spacing(5),
         textAlign: 'center',
     },
@@ -25,12 +27,28 @@ const useStyles = makeStyles((theme) => ({
 
 export default function MapPaperSelection () {
     const classes = useStyles();
+    const [value, setAge] = React.useState('');
+    const [open, setOpen] = React.useState(false);
+
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
 
     return (
         <Paper elevation={3} className={classes.paper}>
             <Typography variant={'h2'}> Choisissez votre destination </Typography>
 
-            <Grid container spacing={3}>
+            <Grid container spacing={1}>
                 <Grid item xs={6}>
                     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className={classes.map}>
                         <TileLayer
@@ -43,11 +61,33 @@ export default function MapPaperSelection () {
                             </Popup>
                         </Marker>
                     </MapContainer>
+
                 </Grid>
 
                 <Grid item xs={6}>
+
+                    <Typography display="inColomn">U</Typography>
+                    <Typography display="inColomn">N</Typography>
+                    <Typography display="inColomn">I</Typography>
+                    <Typography display="inColomn">V</Typography>
+                    <Typography display="inColomn">E</Typography>
+                    <Typography display="inColomn">R</Typography>
+                    <Typography display="inColomn">S</Typography>
+                    <Typography display="inColomn">I</Typography>
+                    <Typography display="inColomn">T</Typography>
+                    <Typography display="inColomn">E</Typography>
+
+
                     <Typography variant={'h5'}>Universités</Typography>
+                    <br/>
+                    <Select labelId="universite" id="select" value={value} open={open} onClose={handleClose} onOpen={handleOpen} onChange={handleChange} >
+                        <MenuItem value="10">U1</MenuItem>
+                        <MenuItem value="20">U2</MenuItem>
+                        <MenuItem value="30">U3</MenuItem>
+                        <MenuItem value="40">U3</MenuItem>
+                    </Select>
                 </Grid>
+
             </Grid>
 
         </Paper>
--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../Assets/Style/App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import {Button, Container, CssBaseline, Toolbar, Typography} from "@material-ui/core";
+import {Button, Container, CssBaseline, Grid, Toolbar, Typography} from "@material-ui/core";
 import MapPaperSelection from "../Component/MapPaperSelection";
 import PlaneIcon from "../Assets/Icon/PlaneIcon";
+import Box from "@material-ui/core/Box";
+import BorderColorTwoToneIcon from '@material-ui/icons/BorderColorTwoTone';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,41 +29,94 @@ const useStyles = makeStyles((theme) => ({
         padding: theme.spacing(2),
         textAlign: 'center',
     },
+
+    Box: {
+        backgroundColor: 'black',
+        color: '#c59b08',
+        padding: 10,
+        //textAlign: 'center',
+        //width: '200px',
+        //height: '200px',
+        margin: '20px',
+        borderWidth: '3px',
+        borderRadius: '5px 5px 5px 5px',
+        opacity: 0.75,
+        background: "whitesmoke",
+        font: 'small-caps bold 24px/1 sans-serif',
+        textDecoration: 'overline',
+
+    },
+    Box2: {
+        backgroundColor: 'white',
+        color: '#c59b08',
+        padding: 10,
+        //textAlign: 'center',
+        //width: '200px',
+        //height: '200px',
+        margin: '20px',
+        borderWidth: '3px',
+        borderRadius: '5px 5px 5px 5px',
+        opacity: 0.75,
+        background: "whitesmoke",
+        font: 'small-caps bold 24px/1 sans-serif',
+        textDecoration: 'overline',
+    },
+
+    Box3: {
+        backgroundColor: 'black',
+        color: '#c59b08',
+        padding: 10,
+        margin: '20px',
+        borderWidth: '5px',
+        borderRadius: '5px 5px 5px 5px',
+        opacity: 0.75,
+        background: "whitesmoke",
+        font: 'small-caps bold 24px/1 sans-serif',
+
+    }
 }));
 
-function Home() {
+export default function Home() {
 
     const classes = useStyles();
     return (
-        <div className={classes.root}>
+        <Box component ="div" className="note">
 
-            <div className={classes.title}>
-                <h1> Mobilités Internationales </h1>
-                <h2> Découvrez les avis des étudiants revenus d&apos;échange</h2>
-            </div>
+            <Box className={classes.Box}>
+                <Typography variant="h3"> Mobilités Internationales </Typography>
+                <Typography variant="h4"> Découvrez les avis des étudiants revenus d&apos;échange</Typography>
+            </Box>
+            <Box className= {classes.Box2}>
+                <Container >
+                    <MapPaperSelection/>
+                </Container>
+            </Box>
 
-            <Container>
-                <MapPaperSelection/>
-            </Container>
-
-            <div className={classes.experience}>
-                <Typography variant={'h3'}>Partagez votre expérience</Typography>
+            <Box component="div" className={classes.Box3}  >
+                <Typography variant={'h4'}>PARTAGER VOTRE EXPERIENCE</Typography>
                 <PlaneIcon />
 
-                <Typography variant={'h4'}>
-                    Vous revenez d&apos;échange ? <br/>
-                    Aidez les étudiants dans leur choix en<br/>
-                    partageant votre expérience et en<br/>
-                    répondant à leurs questions.
-                </Typography>
-                <Button variant="contained" color="secondary" href="experience">
-                    Donner mon avis
-                </Button>
-            </div>
+                <Grid container spacing={1}>
+                    <Grid item xs={6} sm = {7}>
+                        <Typography variant={'h6'}>
+                            Vous revenez d&apos;échange ? <br/>
+                            Aidez les étudiants dans leur choix en<br/>
+                            partageant votre expérience et en<br/>
+                            répondant à leurs questions.
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} sm ={2} >
+                        <Button variant="contained" color="primary" href="experience">
+                            Donner mon avis
+                            <BorderColorTwoToneIcon></BorderColorTwoToneIcon>
+                        </Button>
 
-        </div>
+                    </Grid>
+                </Grid>
+            </Box>
+
+        </Box>
     );
 }
 
-export default Home;
 

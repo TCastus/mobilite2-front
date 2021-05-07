2b96bedc3f048a7cb4263bded70ce4078e021e0a
Estelle <esmonier01@gmail.com>
2021-05-07 18:03:58 +0200

    fin du redesign

--- src/Views/Experience.js
+++ src/Views/Experience.js
@@ -6,7 +6,9 @@ import Typography from "@material-ui/core/Typography";
 import '../Assets/Style/style.css';
 import SecurityIcon from '@material-ui/icons/Security';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import {TextField} from "@material-ui/core";
+import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import {Container, TextField} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import SendIcon from "@material-ui/icons/Send";
 import FormControl from "@material-ui/core/FormControl";
@@ -19,20 +21,9 @@ import MenuItem from '@material-ui/core/MenuItem';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
 import Input from '@material-ui/core/Input';
+import PageHeader from "../Component/PageHeader";
 
 const useStyles = makeStyles((theme) => ({
-    title: {
-        height: '12vh',
-        color: theme.palette.primary.light,
-        background: 'rgba(100,100,100,100)',
-        margin: '0px',
-        padding: 2,
-        textAlign: 'center',
-        backgroundImage: `url("../Assets/images/plane.svg")`,
-
-
-    },
-
     avis: {
         textAlign: 'center',
         margin: '0px',
@@ -42,30 +33,37 @@ const useStyles = makeStyles((theme) => ({
 
     Box: {
         border: '2px solid white',
-        color: theme.palette.secondary,
         padding: 10,
-        //textAlign: 'center',
-        //width: '200px',
-        //height: '200px',
-        margin: '20px',
+        margin: theme.spacing(1, 'auto'),
         borderWidth: '3px',
         borderRadius: '0px 0px 10px 0px',
         opacity: 0.75,
         background: "whitesmoke",
+
+    },
+    Box2: {
+        color: theme.palette.secondary,
+        padding: 10,
+        margin: theme.spacing(1, 'auto'),
+        borderWidth: '3px',
+        borderRadius: '0px 0px 10px 0px',
+        opacity: 0.75,
+        background: "lightgrey",
+        textAlign: "center",
     },
 
     BoxWhite: {
-        border: '2px solid black',
-        color: theme.palette.secondary,
-        //padding: 20,
-        //textAlign: 'center',
-        //margin: '24px',
-        //borderWidth: '3px',
-        //borderRadius: '0px 0px 10px 0px',
-        background: 'white',
+        padding: 10,
+        margin: theme.spacing(1, 'auto'),
+        background: "white",
         width: '400px'
 
-    }
+    },
+
+
+    items:{
+        margin: theme.spacing(1, 'auto'),
+    },
 
 }));
 
@@ -96,97 +94,166 @@ export default function Experience() {
 
 
     return (
-        <Box component="div" className="header">
-            <Box component="div" className= "note">
-                <Typography>PARTAGER SON EXPERIENCE
-                </Typography>
-            </Box>
+        <div className={classes.root}>
+            <PageHeader title={"Partagez votre expérience"}/>
+            <Container maxWidth="md">
+                <Box component="div" className="header">
+                    <Box component="div">
+                        <Grid component="div" className={classes.Box} >
+                            <Typography variant={'h4'} className={classes.items}>Vous revenez d&apos;échange ?</Typography>
+                            <Typography variant={'h5'} className={classes.items}>Remplissez le formulaire ci-dessous et aidez les futures<br/> générations à faire leur choix !</Typography>
 
-            <Box component="div">
-                <Grid component="div" className={classes.Box} background="white">
-                    <Grid container spacing={1}>
-                        <Grid item xs={12} spacing={3}>
-                            <InputLabel htmlFor="Prénom :" color="black">Prenom </InputLabel>
-                            <Input type="text" name="Prénom" id="Prénom" placeholder="Votre pseudo"/>
+                            <Grid container className={classes.items}>
+                                <Grid item xs={6}>
+                                    <Grid container spacing={1} className={classes.items}>
+                                        <Grid item xs={12} spacing={3}>
+                                            <Typography> Prénom </Typography>
+                                            <Input type="text" name="Prénom" id="Prénom" placeholder="Votre prénom"/>
+                                        </Grid>
+                                        <Grid item xs={12} spacing={3}>
+                                            <Typography> Nom de famille </Typography>
+                                            <Input type="text" name="Nom de Famille" id="Nom de Famille" placeholder="Votre nom"/>
+                                        </Grid>
+                                        <Grid item xs={12} spacing={3}>
+                                            <Typography id="depart">Département</Typography>
+                                            <Select labelId="depart" id="select" value={value} open={open} onClose={handleClose} onOpen={handleOpen} onChange={handleChange}>
+                                                <MenuItem value="10">TC</MenuItem>
+                                                <MenuItem value="20">IF</MenuItem>
+                                                <MenuItem value="30">BS</MenuItem>
+                                                <MenuItem value="40">SGM</MenuItem>
+                                                <MenuItem value="50">GCU</MenuItem>
+                                                <MenuItem value="60">GE</MenuItem>
+                                                <MenuItem value="70">GEN</MenuItem>
+                                                <MenuItem value="80">GI</MenuItem>
+                                                <MenuItem value="90">GM</MenuItem>
+                                            </Select>
+                                        </Grid>
+                                        <Grid item xs={12} spacing={3}>
+                                            <Typography>Année d&apos;échange </Typography>
+                                            <Input type="text" name="Annee" id="Annee" placeholder="ex : 2016"/>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+
+                                <Grid item xs={6} spacing={3}>
+                                    <Grid container>
+                                        <Grid item xs={12}>
+                                            <Typography> Commentaire </Typography>
+                                            <TextField className={classes.BoxWhite} style={{textAlign: 'left'}} placeholder="ex : J'ai beaucoup aimé cet échange, j'ai très bien été acceuilli..." hintText="Message Field" floatingLabelText="MultiLine and FloatingLabel" multiline rows={5}/>
+                                        </Grid>
+                                        <Grid item xs={12} spacing={3} className={classes.items}>
+                                            <Typography>Email</Typography>
+                                            <Input type="text" name="Email" id="Email" placeholder="ex : toto@email.fr"/>
+                                        </Grid>
+                                        <Grid item xs={12}>
+                                            <Typography textAlign="center">
+                                                Souhaitez-vous que nous partagions votre e-mail<br/>
+                                                aux élèves intéressés par votre échange souhaitant vous poser des questions ?
+                                            </Typography>
+                                            <FormControl component="fieldset">
+                                                <RadioGroup row aria-label="position" name="position" defaultValue="end">
+                                                    <FormControlLabel value="end" control={<Radio color="primary" checked={selectedValue === 'a'} onChange={handleChange2} value="a" name="radio-button-demo" inputProps={{ 'aria-label': 'A' }}/>} label="Oui" />
+                                                    <FormControlLabel value="end" control={<Radio color="primary" checked={selectedValue === 'b'} onChange={handleChange2} value="b" name="radio-button-demo" inputProps={{ 'aria-label': 'B' }}/>} label="Non" />
+                                                </RadioGroup>
+                                            </FormControl>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} spacing={3}>
-                            <InputLabel htmlFor="Nom de Famille :">Nom de Famille </InputLabel>
-                            <Input type="text" name="Nom de Famille" id="Nom de Famille" placeholder="Nom"/>
+
+                    </Box>
+
+                    <Box component="div" maxWidth="sm">
+                        <Grid container className={classes.Box2}>
+                            <Grid item xs={3}>
+                                <Container maxWidth="md">
+                                    <Typography variant={'h6'} className={classes.Box}>
+                                        <Grid container>
+                                            <Grid item xs={12}>
+                                                <SecurityIcon> </SecurityIcon>
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                Sécurité
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Rating id="sécu" name="hover-feedback" /*value={value} */ precision={0.5}/>
+                                            </Grid>
+                                        </Grid>
+                                    </Typography>
+                                </Container>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Container maxWidth="sm">
+                                    <Typography variant={'h6'} className={classes.Box}>
+                                        <Grid container>
+                                            <Grid item xs={12}>
+                                                <AttachMoneyIcon> </AttachMoneyIcon>
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                Coût de la vie
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Rating id="cout" name="hover-feedback2"/*value={value}*/ precision={0.5} color={blueGrey}/>
+                                            </Grid>
+                                        </Grid>
+                                    </Typography>
+                                </Container>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Container maxWidth="sm">
+                                    <Typography variant={'h6'} className={classes.Box}>
+                                        <Grid container>
+                                            <Grid item xs={12}>
+                                                <AccountBoxIcon> </AccountBoxIcon>
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                Vie culturelle
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Rating id="social" name="hover-feedback3"/*value={value}*/ precision={0.5} color={blueGrey}/>
+                                            </Grid>
+                                        </Grid>
+                                    </Typography>
+                                </Container>
+                            </Grid>
+                            <Grid item xs={3}>
+                                <Container maxWidth="sm">
+                                    <Typography variant={'h6'} className={classes.Box}>
+                                        <Grid container>
+                                            <Grid item xs={12}>
+                                                <EmojiPeopleIcon> </EmojiPeopleIcon>
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                Vie nocturne
+                                            </Grid>
+                                            <Grid item xs={12}>
+                                                <Rating id="nuit" name="hover-feedback4"/*value={value}*/ precision={0.5} color={blueGrey}/>
+                                            </Grid>
+                                        </Grid>
+                                    </Typography>
+                                </Container>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography variant={'h4'}>
+                                    Note globale
+                                    <Grid item xs={12}>
+                                        <Rating id="note" name="hover-feedback5"/*value={value}*/ precision={0.5} color={blueGrey}/>
+                                    </Grid>
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12} className={classes.items}>
+                                <Box component="div">
+                                    <Button aria-label="submit" variant="contained" edge="start" color="secondary" component="span">
+                                        <InputLabel> Submit </InputLabel>
+                                        <SendIcon/>
+                                    </Button>
+                                </Box>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={12} spacing={3}>
-                            <InputLabel id="depart">Département</InputLabel>
-                            <Select labelId="depart" id="select" value={value} open={open} onClose={handleClose} onOpen={handleOpen} onChange={handleChange}
-                            >
-                                <MenuItem value="10">TC</MenuItem>
-                                <MenuItem value="20">IF</MenuItem>
-                                <MenuItem value="30">BS</MenuItem>
-                                <MenuItem value="40">SGM</MenuItem>
-                                <MenuItem value="50">GCU</MenuItem>
-                                <MenuItem value="60">GE</MenuItem>
-                                <MenuItem value="70">GEN</MenuItem>
-                                <MenuItem value="80">GI</MenuItem>
-                                <MenuItem value="90">GM</MenuItem>
-                            </Select>
-                        </Grid>
-                        <Grid item xs={12} spacing={3}>
-                            <InputLabel textAlign="center">Souhaitez-vous que nous partagions votre e-mail
-                                aux élèves intéressés par votre échange souhaitant vous poser des questions
-                                ? </InputLabel>
-                            <FormControl component="fieldset">
-                                <RadioGroup row aria-label="position" name="position" defaultValue="end">
-                                    <FormControlLabel value="end" control={<Radio color="primary" checked={selectedValue === 'a'} onChange={handleChange2} value="a" name="radio-button-demo" inputProps={{ 'aria-label': 'A' }}/>} label="Oui" />
-                                    <FormControlLabel value="end" control={<Radio color="primary" checked={selectedValue === 'b'} onChange={handleChange2} value="b" name="radio-button-demo" inputProps={{ 'aria-label': 'B' }}/>} label="Non" />
-                                </RadioGroup>
-                            </FormControl>
-
-                        </Grid>
-                    </Grid>
-                </Grid>
-
-            </Box>
-
-            <Box component="div">
-                <Typography className={classes.Box}>
-                    <SecurityIcon></SecurityIcon>
-                    <InputLabel htmlFor="sécu"> Sécurité </InputLabel>
-                    <br/>
-                    <Rating id="sécu"
-                        name="hover-feedback"
-                        //value={value}
-                        precision={0.5}
-
-                    />
-                    <br/>
-                    <AccountBoxIcon></AccountBoxIcon>
-                    <InputLabel htmlFor="social"> Contact Humain </InputLabel>
-                    <br/>
-                    <Rating id="social" name="hover-feedback2"
-                        //value={value}
-                        precision={0.5}
-                        color={blueGrey}
-
-                    />
-                    <br/>
-                </Typography>
-            </Box>
-
-            <Box component="div" className={classes.Box}>
-                <Typography>
-                    Commentaire
-                </Typography>
-                <br/>
-                <TextField className={classes.BoxWhite} style={{textAlign: 'left'}} hintText="Message Field"
-                    floatingLabelText="MultiLine and FloatingLabel" multiline rows={5}/>
-            </Box>
-
-            <Box component="div">
-                <Button aria-label="submit" variant="contained" edge="start" color="secondary" component="span">
-                    <InputLabel> Submit </InputLabel>
-                    <SendIcon/>
-                </Button>
-            </Box>
-        </Box>
-
-
+                    </Box>
+                </Box>
+            </Container>
+        </div>
     );
 }
